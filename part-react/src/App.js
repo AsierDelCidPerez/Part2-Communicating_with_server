cbc9daecd67d352e7509b5202e598ffdebea7386
Asier175 <86838694+Asier175@users.noreply.github.com>
2022-04-22 15:28:07 +0200

    22/04/200 15:27
    Asier Del Cid Pérez

--- part-react/src/App.js
+++ part-react/src/App.js
@@ -1,16 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 import Note from './components/Note';
 
-const App = ({notes}) => {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a you new note...");
+  let contador = 1;
+  const actualizarNewNote = event => {
+    setNewNote(event.target.value);
+  }
+
+  const addNote = event => {
+    event.preventDefault();
+    setNotes(notes.concat({
+      id: contador,
+      content: newNote,
+      date: new Date().toString(),
+      important: true,
+    }));
+    setNewNote("a you new note...");
+    contador++;
+  }
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {
-          notes.map(note => <Note key={note.id} note={note}/>)
+          notes.map(note => <Note key={contador} note={note}/>)
         }
       </ul>
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={actualizarNewNote}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 }
